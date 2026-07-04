66168e0b6f0d067fb73517fd603aaf781e6fb12e
pragathiraj7 <pragathijacky@gmail.com>
2026-07-04 23:12:12 +0530

    swap2 program

--- lagr3.c
+++ lagr3.c
@@ -8,7 +8,7 @@ int main() {
     if(a >= b && a >= c) {
         printf("%d is the largest number.\n", a);
     } else if(b >= a && b >= c) {
-        printf("%d is the largest number.\n", b);
+        printf("%d is the 3largest number.\n", b);
     } else {
         printf("%d is the largest number.\n", c);
     }
