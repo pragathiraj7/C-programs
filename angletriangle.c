625024e89134657a877b2f6f0f69ea0db06a306e
pragathiraj7 <pragathijacky@gmail.com>
2026-07-05 22:07:02 +0530

    angletriangle

---
+++ angletriangle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int a,b,c;
+void main(){
+    printf("enetr the  first value\n");
+    printf("enetr the  second value\n1");
+    scanf("%d%d",&a,&b);
+    c = 180-(a+b);
+    printf("the value is %d",c);
+}
